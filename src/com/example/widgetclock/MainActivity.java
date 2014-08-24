1059d9f5b323af1460f1803c0ba628c8353bd123
dozenwang <dozenwang@foxmail.com>
2014-08-24 17:20:19 +0800

    添加Umeng sdk
    Signed-off-by: dozenwang <dozenwang@foxmail.com>

--- src/com/example/widgetclock/MainActivity.java
+++ src/com/example/widgetclock/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.widgetclock;
 
 import com.example.widgetclock.R;
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.fb.FeedbackAgent;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
@@ -11,17 +13,38 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends Activity {
 
+	private Button mFeedbackBtn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mFeedbackBtn = (Button) findViewById(R.id.feedback);
+		mFeedbackBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				FeedbackAgent agent = new FeedbackAgent(MainActivity.this);
+				agent.startFeedbackActivity();
+			}
+		});
 	}
-
-
-
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		MobclickAgent.onResume(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		MobclickAgent.onPause(this);
+	}
 }
